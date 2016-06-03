4c7fcf6fb046c8a2be77ef8a89da654e76976706
Tim <tim.bathgate@gmail.com>
2016-06-03 17:39:41 +1200

    Create relevant views
    - Created storyboard elements for Travel Locations view
    - Created storyboard elements for Photo Album view
    - Created view controllers for each view

---
+++ Virtual Tourist/PhotoAlbumViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoAlbumViewController.swift
+//  Virtual Tourist
+//
+//  Created by Tim on 3/06/16.
+//  Copyright © 2016 Tim. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class PhotoAlbumViewController : UIViewController {
+    
+}
---
+++ Virtual Tourist/TravelLocationsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TravelLocationsViewController.swift
+//  Virtual Tourist
+//
+//  Created by Tim on 3/06/16.
+//  Copyright © 2016 Tim. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class TravelLocationsViewController : UIViewController {
+
+}
