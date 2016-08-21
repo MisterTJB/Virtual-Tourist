fdc06b77d58a45268a26d7d24fe8e94e5c7271d1
Tim <tim.bathgate@gmail.com>
2016-08-21 13:30:10 +1200

    Add long press drops pin on main map

--- Virtual Tourist/TravelLocationsViewController.swift
+++ Virtual Tourist/TravelLocationsViewController.swift
@@ -9,6 +9,43 @@
 import UIKit
 import MapKit
 
-class TravelLocationsViewController : UIViewController {
+class TravelLocationsViewController : UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        mapView.delegate = self;
+    }
+    
+    
+    /**
+     Event handler for long-press gestures on the map. Determines the geographic coordinate 
+     for the point under the user's finger and creates a new pin
+    */
+    @IBAction func pressedOnMap(sender: UILongPressGestureRecognizer) {
+        
+        if (sender.state == UIGestureRecognizerState.Ended){
+            let touchCoordinate = sender.locationInView(self.mapView);
+            let mapCoordinate = mapView.convertPoint(touchCoordinate, toCoordinateFromView: self.mapView);
+            addPinToMapAtCoordinate(coordinate: mapCoordinate);
+            
+        }
+    }
+    
+    
+    /**
+     Adds a pin to the main map at a given geographic coordinate
+     
+     - Parameters:
+        - coordinate: The geographic coordinate at which to drop a pin
+     
+     */
+    func addPinToMapAtCoordinate(coordinate coord : CLLocationCoordinate2D){
+        let pin = MKPointAnnotation();
+        pin.coordinate = coord;
+        mapView.addAnnotation(pin);
+    }
+    
+    
 
 }
