aa4f0c7174bf962ba30b5146386b3b28737de9e9
Tim <tim.bathgate@gmail.com>
2016-08-21 17:23:34 +1200

    Add interface builder connections and TODOs for implementing photo download and display

--- Virtual Tourist/PhotoAlbumViewController.swift
+++ Virtual Tourist/PhotoAlbumViewController.swift
@@ -11,7 +11,19 @@ import MapKit
 
 class PhotoAlbumViewController : UIViewController {
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    // TODO Create photos using identifiers and managed object context
+    // i.e. call some function in FlickrDownloadManager, let the callback
+    // create a new Photo(id, imageData), then set its Pin to the current 
+    // Pin.
+    // Also, when that pin is created, display it in the collection view 
+    // somehow
+    
     override func viewDidLoad() {
+        
+        // TODO If FetchResult is empty, download images. Else, load in data
         FlickrDownloadManager.downloadImagesForRegion { data, error in
             
             if let error = error {
@@ -22,4 +34,9 @@ class PhotoAlbumViewController : UIViewController {
         }
     }
     
+    @IBAction func newCollection(sender: UIBarButtonItem) {
+        // TODO The Photo Album view has a button that initiates the download 
+        // of a new album, replacing theimages in the photo album with a new 
+        // set from Flickr.
+    }
 }
